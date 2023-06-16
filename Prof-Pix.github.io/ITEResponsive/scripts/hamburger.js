8481dd4a3154de7f92df6ea7302133d305ce0813
profpix <130376916+Prof-Pix@users.noreply.github.com>
2023-06-16 13:57:43 +0800

    Delete Prof-Pix.github.io directory

--- Prof-Pix.github.io/ITEResponsive/scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".hamburger-menu");
-
-    hamburger.addEventListener('click', () => {
-        navBar = document.querySelector(".nav-bar");
-        navBar.classList.toggle("active");
-
-        hamburger.classList.toggle("active");
-    })
